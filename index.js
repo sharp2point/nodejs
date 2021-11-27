a4226848156987049aa68fb4cfc15dd5e816873f
Evgeny Blinov <sharppoint@yandex.ru>
2021-11-28 00:58:55 +0300

    add: Handlebars templates

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express')
+const { engine } = require('express-handlebars');
 
 const app = express()
 const port = process.env.PORT || 3000
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+
+
+
+app.get('/',(req,res)=> res.render('home'))
+
+app.get('/about', (req,res)=> res.render('about'))
 
 app.use((req,res)=>{
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - not found')
-})
+    res.render('404')})
 
 app.use((err,req,res,next)=>{
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - server error')
+    res.render('500')
 })
 
 app.listen(port, ()=>console.log(
-    `Express run`
+    `Express run server on port: ${port}`
 ))
